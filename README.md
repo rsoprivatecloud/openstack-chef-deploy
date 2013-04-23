e7669f0c7e54f450e04eb10ded7473f6a53a77e8
David Wittman <david@wittman.com>
2013-04-23 02:54:02 -0500

    Remove Berkshelf, add some other stuff
    * Berkshelf was providing few benefits over the (now tagged)
      github repositories, so we're just going to keep it simple.
    * Remove chef-client and bootstrap-version from spiceweasel
    * Convert more constants to task arguments
    * Implement timeout for Chef key regeneration command

--- README.md
+++ README.md
@@ -1,34 +1,19 @@
 # Deploying OpenStack with Chef Cookbooks
-This project aims to make Chef deployments of OpenStack infrastructures easier through the use of [Berkshelf](http://berkshelf.com) and [Spiceweasel](https://github.com/mattray/spiceweasel).
+This project aims to make Chef deployments of OpenStack infrastructures easier through the use of [fabric](http://fabfile.org) and [Spiceweasel](https://github.com/mattray/spiceweasel).
 
 ## Overview
 
-### Berkshelf
-[Berkshelf](http://berkshelf.com/) is the [Bundler](http://gembundler.com/) equivalent for Chef cookbooks. Just as Bundler has a `Gemfile`, Berkshelf allows us to manage cookbook dependencies for a project in a `Berkfile`, which can easily be uploaded to Chef Server.
+### Fabric
+Some stuff about fabric
 
 ### Spiceweasel
 [Spiceweasel](https:////github.com/mattray/spiceweasel) is a command-line tool for bootstrapping Chef infrastructures. Rather than run individual commands with knife, Spiceweasel allows you to define a template in YAML or JSON for your infrastructure, which it then converts to the appropriate knife commands.
 
 ## Getting Started
-This document assumes that you already have Chef Server installed and a registered client with Admin access. These commands are intended to be run as a user with appropriate privileges and valid configurations in their `knife.rb` file. For more information, check out the Opscode Chef documentation.
 
-Before anything else, you should clone this repository and `cd` into the created directory. After that, you're ready to proceed with the Berkshelf and Spiceweasel configurations.
+### Fabric
+#### Install fabric
 
-``` bash
-$ git clone https://github.com/DavidWittman/openstack-chef-deploy
-$ cd openstack-chef-deploy
-```
-
-### Uploading cookbooks with Berkshelf
-#### Install Berkshelf
-``` bash
-$ gem install berkshelf
-```
-
-#### Upload cookbooks to Chef
-``` bash
-$ berks upload
-```
 
 ### Spiceweasel
 #### Install Spiceweasel
--- fabfile.py
+++ fabfile.py
@@ -6,6 +6,7 @@ import time
 from fabric.api import env, run, put, cd, prompt, sudo, roles
 from fabric.colors import red, yellow, green
 from fabric.contrib import files
+from fabric.exceptions import CommandTimeout
 from fabric.utils import puts
 
 # needed to pick up ProxyCommand from config
@@ -17,20 +18,20 @@ def install_kvm():
     sudo('apt-get -qq update')
     sudo('apt-get install -yq qemu-kvm libvirt-bin python-libvirt curl')
     
-def install_chef_server():
-    "Installs and configures Chef Server on the controller node"
-    chef_server_xml = 'files/chef-server.xml'
-    chef_server_image = 'http://c390813.r13.cf1.rackcdn.com/chef-server.qcow2'
-    assert os.path.exists(chef_server_xml), 'Chef Server XML definition not found at %s' % chef_server_xml
+def create_chef_server(url='http:////c390813.r13.cf1.rackcdn.com/chef-server.qcow2',
+                       xml_definition='files/chef-server.xml',
+                       mgmt_network='10.240.0.0/24'):
+    "Creates and configures Chef Server on the controller node"
+    assert os.path.exists(xml_definition), 'Chef Server XML definition not found at %s' % xml_definition
 
     install_kvm()
     sudo('echo iptables-persistent iptables-persistent/autosave_v6 boolean true | debconf-set-selections')
     sudo('echo iptables-persistent iptables-persistent/autosave_v4 boolean true | debconf-set-selections')
     sudo('apt-get install -yq iptables-persistent')
 
-    puts(green('Downloading Chef Server image from %s' % chef_server_image))
+    puts(green('Downloading Chef Server image from %s' % url))
     sudo('mkdir /opt/rpcs', warn_only=True)
-    sudo('wget -qO /opt/rpcs/chef-server.qcow2 %s' % chef_server_image)
+    sudo('wget -qO /opt/rpcs/chef-server.qcow2 %s' % url)
     sudo('cp -a /opt/rpcs/chef-server.qcow2{,.pristine}')
     
     puts(green('Configuring Chef Server'))
@@ -38,7 +39,7 @@ def install_chef_server():
     files.uncomment('/etc/sysctl.conf', r'net\.ipv4\.ip_forward=1', use_sudo=True)
     sudo('sysctl net.ipv4.ip_forward=1')
 
-    put(chef_server_xml, '/opt/rpcs/chef-server.xml', use_sudo=True)
+    put(xml_definition, '/opt/rpcs/chef-server.xml', use_sudo=True)
     files.append('/etc/network/interfaces', """
         auto chefbr0
         iface chefbr0 inet static
@@ -56,8 +57,7 @@ def install_chef_server():
     sudo('virsh autostart chef-server && virsh start chef-server')
      
     puts(green('Configuring NAT rules'))
-    management_network = prompt('What is the MANAGEMENT network range?', default='10.240.0.0/24')
-    sudo('iptables -t nat -A PREROUTING -s %s -p tcp --dport 4000 -j DNAT --to-dest 169.254.123.2' % management_network)
+    sudo('iptables -t nat -A PREROUTING -s %s -p tcp --dport 4000 -j DNAT --to-dest 169.254.123.2' % mgmt_network)
     sudo('iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE')
     sudo('service iptables-persistent save')
 
@@ -71,11 +71,21 @@ def install_chef_server():
 
     puts(green('Creating Chef validation and controller client keys'))
     sudo('mkdir -p /root/.chef', warn_only=True)
-    sudo('ssh -i /root/.ssh/id_rsa rack@169.254.123.2 "knife client reregister rack -f .chef/rack.pem; knife client reregister chef-validator -f .chef/validation.pem; sudo cp .chef/validation.pem /usr/share/chef-server-api/public; sudo chmod +r /usr/share/chef-server-api/public/validation.pem; yes | knife client delete controller &> /dev/null; knife environment create -d rpcs &>/dev/null; knife client create controller -d -a" | tail -n+2 > /root/.chef/controller.pem')
 
-def configure_knife(chef_server_url='http://169.254.123.2:4000/'):
+    retry = True
+    long_ass_ssh_command = 'ssh -i /root/.ssh/id_rsa rack@169.254.123.2 "knife client reregister rack -f .chef/rack.pem; knife client reregister chef-validator -f .chef/validation.pem; sudo cp .chef/validation.pem /usr/share/chef-server-api/public; sudo chmod +r /usr/share/chef-server-api/public/validation.pem; yes | knife client delete controller &> /dev/null; knife environment create -d rpcs &>/dev/null; knife client create controller -d -a" | tail -n+2 > /root/.chef/controller.pem'
+    while retry is True:
+        sleep_time = 10
+        try:
+            sudo(long_ass_ssh_command, warn_only=True, timeout=15)
+            retry = False
+        except CommandTimeout:
+            puts(yellow('Timeout connecting to Chef Server. Retrying in %ss...' % sleep_time))
+            time.sleep(sleep_time)
+ 
+def configure_knife(chef_server_url='http://169.254.123.2:4000/',
+                    knife_template='files/knife.rb.tpl'):
     "Installs Chef and configures Knife"
-    knife_template = 'files/knife.rb.tpl'
     assert os.path.exists(knife_template), 'Knife configuration template not found at %s' % knife_template
     
     if not chef_server_url.endswith('/'):
@@ -84,7 +94,7 @@ def configure_knife(chef_server_url='http://169.254.123.2:4000/'):
         chef_server_url = 'http://' + chef_server_url
 
     puts(green('Installing Chef'))
-    sudo('bash <(curl -sL http://opscode.com/chef/install.sh) -v 10.20.0')
+    sudo('bash <(curl -sL http://opscode.com/chef/install.sh)')
 
     puts(green('Creating knife config'))
     sudo('mkdir -p /root/.chef', warn_only=True)
@@ -93,20 +103,20 @@ def configure_knife(chef_server_url='http://169.254.123.2:4000/'):
     sudo('mkdir -p /etc/chef', warn_only=True)
     sudo('wget -nv %svalidation.pem -O /etc/chef/validation.pem' % chef_server_url)
 
-def upload_cookbooks():
+def upload_cookbooks(url="http://github.com/rcbops/chef-cookbooks",
+                     branch="v3.0.1"):
     "Uploads OpenStack Chef cookbooks"
-    git_url = "https://github.com/DavidWittman/openstack-chef-deploy"
-    directory = "/opt/rpcs/openstack-chef-deploy"
+    directory = "/opt/rpcs/chef-cookbooks"
 
     puts(green("Installing dependencies"))
-    sudo('apt-get install -qy git ruby1.9.3 build-essential')
-    sudo('gem install berkshelf spiceweasel --no-ri --no-rdoc')
+    sudo('apt-get install -qy git')
 
     if files.exists(directory):
         sudo('rm -rf %s' % directory)
 
-    sudo('git clone %s %s' % (git_url, directory))
+    puts('Cloning chef-cookbooks repository')
+    sudo('git clone -q --recursive -b %s %s %s' % (branch, url, directory))
 
-    puts(green("Uploading cookbooks"))
-    with cd(directory):
-        sudo('BERKSHELF_CHEF_CONFIG=/root/.chef/knife.rb berks upload')
+    puts(green("Uploading cookbooks and roles"))
+    sudo('knife cookbook upload -c /root/.chef/knife.rb -a')
+    sudo('knife role from file /opt/rpcs/chef-cookbooks/roles/*.rb -c /root/.chef/knife.rb')
