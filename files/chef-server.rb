25d22b5240b5277415dd4e1ddd571e3969a77a94
David Wittman <david.wittman@rackspace.com>
2013-07-04 18:50:38 -0500

    Use IP address instead of FQDN for bookshelf url

--- files/chef-server.rb
+++ files/chef-server.rb
@@ -3,3 +3,4 @@ nginx["non_ssl_port"] = 4080
 nginx["enable_non_ssl"] = true
 rabbitmq["enable"] = false
 rabbitmq["password"] = "chuc93prethugucR"
+bookshelf['url'] = "https://#{node['ipaddress']}:4000"
