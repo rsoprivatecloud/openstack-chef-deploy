b8db3ae8a4eed530595c09fcec313c3b832877ef
David Wittman <david.wittman@rackspace.com>
2013-08-26 17:48:22 -0500

    Begin conversion to Bash

--- files/chef-server.rb
+++
@@ -1,6 +0,0 @@
-nginx["ssl_port"] = 4000
-nginx["non_ssl_port"] = 4080
-nginx["enable_non_ssl"] = true
-rabbitmq["enable"] = false
-rabbitmq["password"] = "chuc93prethugucR"
-bookshelf['url'] = "https://#{node['ipaddress']}:4000"
